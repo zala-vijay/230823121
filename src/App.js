281bce5712e8a14e90300f0a99b242fe614cc547
zala-vijay <zalavijay86770@gmail.com>
2024-03-19 10:10:55 +0530

    update component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Icon from './icon_bar.js';
+function App(props) {
+  return (
+    <>
+      
+      {/* <div style={{"backgroundColor":"black","height":"30px"}}>
+        <button style={{"float":"right","marginTop":"0px","backgroundColor":"black","color":"white"}}>copy</button>
+        <p style={{"color":"white","margin":"0px","paddingTop":"2px","fontSize":"16px"}}>{props.title}</p>
+      </div>
+      <div style={{"backgroundColor":"gray","height":"60px"}}>
+        <p style={{"color":"white","margin":"0px","paddingTop":"3px","height":"10px","fontSize":"15px"}}>{props.content}</p>
+      </div> */}
+      <Icon />
+    </>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/icon_bar.js
@@ -0,0 +1,20 @@
+import './icon_bar.css';
+import './user_icon.svg';
+import  './bell_icon.svg';
+import './massage_icon.svg';
+import './account_icon.svg';
+function Icon() {
+    return (
+      <>
+        
+        <div class="navbar">
+            <a href="#" class="photo icon"></a>
+            <a href="#" class="notification icon"></a>
+            <a href="#" class="message icon"></a>
+            <a href="#" class="login icon"></a>
+        </div>
+      </>
+    );
+  }
+  
+  export default Icon;
